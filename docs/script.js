56f831074aa3c02cf98312dc9cb7279f3e3feeaf
pierryjaquilla <pierry_2808@hotmail.com>
2021-09-02 22:35:06 +0200

    update js for openclose menu

--- docs/script.js
+++ docs/script.js
@@ -8,6 +8,13 @@ function menuOpenCloses() {
     console.log(menuButton);
     console.log(full);
     menuButton.addEventListener("mousedown", function() {
+        if (full[0].classList.contains('hideMenu')) {
+            full[0].classList.remove("hideMenu");
+        } else {
+            full[0].classList.add("hideMenu");
+        }
+    });
+    jQuery(".fullMenu span").on("mousedown", function() {
         full[0].classList.remove("hideMenu");
     });
 }
