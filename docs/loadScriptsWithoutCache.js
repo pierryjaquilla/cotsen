3a8fbd75ff5c8988d2bc8fb345471e598ba416dc
Tibor Udvari <tibor.udvari@icloud.com>
2022-05-28 21:58:10 +0200

    Load from local

--- docs/loadScriptsWithoutCache.js
+++ docs/loadScriptsWithoutCache.js
@@ -3,8 +3,13 @@
 var loaded = document.body.getAttribute("data-loadedcustom");
 
 if (!loaded) {
+    // Load the local one if local
+
     var scr = document.createElement("script");
-    scr.src = "https://pierryjaquilla.github.io/cotsen/script.js" + "?ts=" + new Date().getTime();
+    var isRemote = jQuery.cookie("local") == null;
+    var prefix = isRemote ? "https://pierryjaquilla.github.io/cotsen/" ? "https:localhost:8888/";
+
+    scr.src = prefix + "script.js" + "?ts=" + new Date().getTime();
     document.getElementsByTagName("head")[0].appendChild(scr);
     document.body.setAttribute("data-loadedcustom", true);
 }
--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,3 @@
-
 // var cssFile = document.createElement('link');
 // cssFile.rel = 'stylesheet';
 // cssFile.href = "http://localhost:8888/docs/style.css";  // or path for file {themes('/styles/mobile.css')}
@@ -9,207 +8,237 @@
 // cssFile2.href = "http://localhost:8888/docs/mediaQueryMax840.css";  // or path for file {themes('/styles/mobile.css')}
 // document.body.appendChild(cssFile2); // append css to body element, normally should be head
 
-
 if (document.location.pathname == "/") {
-    window.location.href = "https://cotsen.drupal.gwu.edu/homepage-content";
+  window.location.href = "https://cotsen.drupal.gwu.edu/homepage-content";
 }
 
 console.log("Tibor: Script without cache loaded");
 
-
 window.onload = function () {
-    console.log("Tibor: Window onload happened");
+  console.log("Tibor: Window onload happened");
 
-    loadStyles();
-    menuOpenCloses();
-    changeColorBackgroundMedia();
-    openLibrary();
-    zoomInPicture();
-    accordionMenu();
-    createArrows();
-    changeTemplate1();
-    // getSetCurrentTab();
-    makeStickyPossible();
+  loadStyles();
+  setupIntersectionObserver();
+  menuOpenCloses();
+  changeColorBackgroundMedia();
+  openLibrary();
+  zoomInPicture();
+  accordionMenu();
+  createArrows();
+  changeTemplate1();
+  // getSetCurrentTab();
+  makeStickyPossible();
+};
+
+function setupIntersectionObserver() {
+    console.log("Setup intersection observer")
+  var supported = "IntersectionObserver" in window;
+  if (!supported) return;
+
+  let options = {
+    //root: document.querySelector(".template-1"),
+    rootMargin: "0px",
+    threshold: 0.0,
+  };
+
+  function handleIntersection(entries) {
+    entries.map((entry) => {
+      console.log("intersecting");
+      if (entry.isIntersecting) {
+        entry.target.classList.add("animation-slideIn");
+      }
+    });
+  }
+
+  const target = document.querySelector(".template-1 img");
+
+  var observer = new IntersectionObserver(handleIntersection, options);
+  observer.observe(target);
+
+  window.observer = observer;
 }
 
 function injectStyle(url) {
-    var styleEl = document.createElement("link");
-    styleEl.href = url + "?ts=" + new Date().getTime();
-    styleEl.rel = 'stylesheet';
-    document.getElementsByTagName("head")[0].appendChild(styleEl);
-    console.log("Tibor: Load style " + url); 
+  var styleEl = document.createElement("link");
+  styleEl.href = url + "?ts=" + new Date().getTime();
+  styleEl.rel = "stylesheet";
+  document.getElementsByTagName("head")[0].appendChild(styleEl);
+  console.log("Tibor: Load style " + url);
 }
 
 function loadStyles() {
-    console.log("Tibor: loading styles");
-    var localStyles = ["https://localhost:8888/docs/style.css", "https://localhost:8888/docs/mediaQueryMax840.css"];
-    var remoteStyles = ["https://pierryjaquilla.github.io/cotsen/style.css", "https://pierryjaquilla.github.io/cotsen/mediaQueryMax840.css"];
-    if (window.location.href.includes("homepage-content")) {
-        jQuery(".pane-block-11").remove();
-        localStyles.push("https://localhost:8888/docs/styleHomepage.css");
-        remoteStyles.push("https://pierryjaquilla.github.io/cotsen/styleHomepage.css");
-    }
+  console.log("Tibor: loading styles");
+  var localStyles = [
+    "https://localhost:8888/docs/style.css",
+    "https://localhost:8888/docs/mediaQueryMax840.css",
+  ];
+  var remoteStyles = [
+    "https://pierryjaquilla.github.io/cotsen/style.css",
+    "https://pierryjaquilla.github.io/cotsen/mediaQueryMax840.css",
+  ];
+  if (window.location.href.includes("homepage-content")) {
+    jQuery(".pane-block-11").remove();
+    localStyles.push("https://localhost:8888/docs/styleHomepage.css");
+    remoteStyles.push(
+      "https://pierryjaquilla.github.io/cotsen/styleHomepage.css"
+    );
+  }
 
-    var isRemote = jQuery.cookie('local') == null;
+  var isRemote = jQuery.cookie("local") == null;
 
-    // load a special one for the 
+  // load a special one for the
 
-    console.log("Is remote is " + isRemote);
-    console.log(jQuery.cookie('local'))
-    var stylesToLoad = isRemote ? remoteStyles : localStyles;
+  console.log("Is remote is " + isRemote);
+  console.log(jQuery.cookie("local"));
+  var stylesToLoad = isRemote ? remoteStyles : localStyles;
 
-    stylesToLoad.forEach(styleUrl => injectStyle(styleUrl)); 
+  stylesToLoad.forEach((styleUrl) => injectStyle(styleUrl));
 }
 
 function makeStickyPossible() {
-    let all = jQuery(".template-1 .fullwidth .pos-two .backgroundColored");
-    console.log(all);
+  let all = jQuery(".template-1 .fullwidth .pos-two .backgroundColored");
+  console.log(all);
 
-    if (all.length > 0) {
-        
-        all.each(function(idx) {
-            console.log(this);
-            let newDiv = document.createElement("div");
-            newDiv.classList.add("rowContent");
-            let lib = document.createElement("div");
-            lib.classList.add("buttonLibrary");
-            for (let i = 0; i < 4; i++) {
-                lib.appendChild(document.createElement("div"));
-            }
+  if (all.length > 0) {
+    all.each(function (idx) {
+      console.log(this);
+      let newDiv = document.createElement("div");
+      newDiv.classList.add("rowContent");
+      let lib = document.createElement("div");
+      lib.classList.add("buttonLibrary");
+      for (let i = 0; i < 4; i++) {
+        lib.appendChild(document.createElement("div"));
+      }
 
-            let cont = jQuery(this).find("*");
-            console.log(cont);
-            cont.each(function(f) {
-                console.log(this);
-                newDiv.appendChild(this);
-            });
-            newDiv.append(lib);
-            this.append(newDiv);
-        });
-        
-    }
+      let cont = jQuery(this).find("*");
+      console.log(cont);
+      cont.each(function (f) {
+        console.log(this);
+        newDiv.appendChild(this);
+      });
+      newDiv.append(lib);
+      this.append(newDiv);
+    });
+  }
 }
 function getSetCurrentTab() {
-    console.log("set");
-    let s = window.location.href;
-    let a = s.split("/");
-    a = a[3].replaceThisByThat("-", " ");
-    document.getElementById("currentTab").innerHTML = "<p>" + a + "</p>";
+  console.log("set");
+  let s = window.location.href;
+  let a = s.split("/");
+  a = a[3].replaceThisByThat("-", " ");
+  document.getElementById("currentTab").innerHTML = "<p>" + a + "</p>";
 }
 
 function changeTemplate1() {
-    if (jQuery(".template-1").toArray().length > 0) {
-        let containers = jQuery(".container").toArray();
-        let enable = false;
-        for (let i = 0; i < containers.length; i++) {
-            if (jQuery(containers[i]).hasClass("fullwidth")) {
-                enable = true;
-            }
-            if (enable) {
-                jQuery(containers[i]).addClass("fullwidth");
-                jQuery(".template-1").append(containers[i]);
-                // console.log("yes");
-            }
-        }
-        // console.log(containers);
+  if (jQuery(".template-1").toArray().length > 0) {
+    let containers = jQuery(".container").toArray();
+    let enable = false;
+    for (let i = 0; i < containers.length; i++) {
+      if (jQuery(containers[i]).hasClass("fullwidth")) {
+        enable = true;
+      }
+      if (enable) {
+        jQuery(containers[i]).addClass("fullwidth");
+        jQuery(".template-1").append(containers[i]);
+        // console.log("yes");
+      }
     }
+    // console.log(containers);
+  }
 }
 
 function changeColorBackgroundMedia() {
-    jQuery(".backgroundColored").each(function() {
-        var color = jQuery(this).find(".colorForBackground").html();
-        // console.log(color);
-        if (color) {
-            if (color.includes("#")) {
-                color = "#" + color.split('#')[1];
-                jQuery(this).css("backgroundColor", color);
-            }
-        }
-    });
+  jQuery(".backgroundColored").each(function () {
+    var color = jQuery(this).find(".colorForBackground").html();
+    // console.log(color);
+    if (color) {
+      if (color.includes("#")) {
+        color = "#" + color.split("#")[1];
+        jQuery(this).css("backgroundColor", color);
+      }
+    }
+  });
 }
 
 function menuOpenCloses() {
-    var menuButton = document.getElementById("menuButton");
-    var full = document.getElementsByClassName("fullMenu");
-    // console.log(menuButton);
-    // console.log(full);
-    menuButton.addEventListener("mousedown", function () {
-        if (full[0].classList.contains('hideMenu')) {
-            full[0].classList.remove("hideMenu");
-        } else {
-            full[0].classList.add("hideMenu");
-        }
-        if (jQuery('.headerWhite').length) {
-            jQuery(".headerWhite").addClass("close");
-            jQuery(".mainMenu").addClass("close");
-        }
-    });
-    jQuery(".menuHamburger").on("mousedown", function () {
-        if (full[0].classList.contains('hideMenu')) {
-            full[0].classList.remove("hideMenu");
-        } else {
-            full[0].classList.add("hideMenu");
-        }
-        if (jQuery('.headerWhite').length) {
-            jQuery(".headerWhite").addClass("close");
-            jQuery(".mainMenu").addClass("close");
-        }
-    });
+  var menuButton = document.getElementById("menuButton");
+  var full = document.getElementsByClassName("fullMenu");
+  // console.log(menuButton);
+  // console.log(full);
+  menuButton.addEventListener("mousedown", function () {
+    if (full[0].classList.contains("hideMenu")) {
+      full[0].classList.remove("hideMenu");
+    } else {
+      full[0].classList.add("hideMenu");
+    }
+    if (jQuery(".headerWhite").length) {
+      jQuery(".headerWhite").addClass("close");
+      jQuery(".mainMenu").addClass("close");
+    }
+  });
+  jQuery(".menuHamburger").on("mousedown", function () {
+    if (full[0].classList.contains("hideMenu")) {
+      full[0].classList.remove("hideMenu");
+    } else {
+      full[0].classList.add("hideMenu");
+    }
+    if (jQuery(".headerWhite").length) {
+      jQuery(".headerWhite").addClass("close");
+      jQuery(".mainMenu").addClass("close");
+    }
+  });
 
-    jQuery(".fullMenu span").on("mousedown", function () {
-        full[0].classList.remove("hideMenu");
-    });
+  jQuery(".fullMenu span").on("mousedown", function () {
+    full[0].classList.remove("hideMenu");
+  });
 }
 
-
 function openLibrary() {
-    // console.log(jQuery(".moreImages img"));
-    // console.log("hi bitch");
-    jQuery(".fullwidth img").each(function () {
-        var element = document.createElement("div");
-        element.classList.add("card");
-        var image = document.createElement("IMG");
-        image.alt = jQuery(this).attr("alt");
-        // image.setAttribute('class', 'photo');
-        image.src = jQuery(this).attr("src");
-        jQuery(element).append(image);
-        jQuery(".images-container").append(element);
-        // console.log(this);
-        jQuery("#biggerPicture img").attr('src', jQuery(this).attr('src'));
-    });
-    jQuery("card").last().addClass("last");
+  // console.log(jQuery(".moreImages img"));
+  // console.log("hi bitch");
+  jQuery(".fullwidth img").each(function () {
+    var element = document.createElement("div");
+    element.classList.add("card");
+    var image = document.createElement("IMG");
+    image.alt = jQuery(this).attr("alt");
+    // image.setAttribute('class', 'photo');
+    image.src = jQuery(this).attr("src");
+    jQuery(element).append(image);
+    jQuery(".images-container").append(element);
+    // console.log(this);
+    jQuery("#biggerPicture img").attr("src", jQuery(this).attr("src"));
+  });
+  jQuery("card").last().addClass("last");
 
-    jQuery(".buttonLibrary").on("click", function () {
-        jQuery(".library").addClass("open");
-        console.log("open");
-    });
-    jQuery(".closeLibrary").on("click", function () {
-        jQuery(".library").removeClass("open");
-        console.log("close");
-
-    });
+  jQuery(".buttonLibrary").on("click", function () {
+    jQuery(".library").addClass("open");
+    console.log("open");
+  });
+  jQuery(".closeLibrary").on("click", function () {
+    jQuery(".library").removeClass("open");
+    console.log("close");
+  });
 }
 
 function zoomInPicture() {
-    jQuery(".card img").on("click", function () {
-        console.log(jQuery(this).attr('src'));
-        jQuery("#biggerPicture img").attr('src', jQuery(this).attr('src'));
-    });
+  jQuery(".card img").on("click", function () {
+    console.log(jQuery(this).attr("src"));
+    jQuery("#biggerPicture img").attr("src", jQuery(this).attr("src"));
+  });
 }
 
 function accordionMenu() {
-    jQuery(".fullMenu .promo-title").on("click", function () {
-        if (window.innerWidth < 840) {
-
-            let toOpen = jQuery(this).parent().next();
-            test = toOpen;
-            if (toOpen.height() == 0) {
-                toOpen.height(toOpen[0].scrollHeight);
-            } else {
-                toOpen.height(0);
-            }
-        }
-    });
+  jQuery(".fullMenu .promo-title").on("click", function () {
+    if (window.innerWidth < 840) {
+      let toOpen = jQuery(this).parent().next();
+      test = toOpen;
+      if (toOpen.height() == 0) {
+        toOpen.height(toOpen[0].scrollHeight);
+      } else {
+        toOpen.height(0);
+      }
+    }
+  });
 }
 
 let images;
@@ -217,39 +246,46 @@ let texts;
 let currentDisplayed = 0;
 
 function createArrows() {
-    jQuery("<div class=\"arrow left\"></div>").insertBefore(".backgroundColored .backgroundColored .col-copy h6");
-    jQuery("<div class=\"arrow right\"></div>").insertAfter(".backgroundColored .backgroundColored .col-copy h6");
-    
-    images = jQuery(".fullwidth img").toArray();
-    texts = jQuery(".fullwidth .backgroundColored .col-copy h6").toArray();
-    if (images.length > 0) {
-        images.unshift(jQuery(".backgroundColored img")[0]); //add to begining
-        texts.unshift(jQuery(".backgroundColored .backgroundColored .col-copy h6")[0]);
-    }
-    // console.log(texts);
-    // console.log(images);
-    allowBrowseLibrary();
+  jQuery('<div class="arrow left"></div>').insertBefore(
+    ".backgroundColored .backgroundColored .col-copy h6"
+  );
+  jQuery('<div class="arrow right"></div>').insertAfter(
+    ".backgroundColored .backgroundColored .col-copy h6"
+  );
+
+  images = jQuery(".fullwidth img").toArray();
+  texts = jQuery(".fullwidth .backgroundColored .col-copy h6").toArray();
+  if (images.length > 0) {
+    images.unshift(jQuery(".backgroundColored img")[0]); //add to begining
+    texts.unshift(
+      jQuery(".backgroundColored .backgroundColored .col-copy h6")[0]
+    );
+  }
+  // console.log(texts);
+  // console.log(images);
+  allowBrowseLibrary();
 }
 
 function allowBrowseLibrary() {
-    jQuery(".arrow left").on("click", function () {
-        console.log(currentDisplayed);
-        ++currentDisplayed;
-        console.log(currentDisplayed);
-        jQuery(".backgroundColored img").attr('src', images[currentDisplayed]);
-        jQuery(".backgroundColored .backgroundColored .col-copy h6").text(texts[currentDisplayed]);
-    });
+  jQuery(".arrow left").on("click", function () {
+    console.log(currentDisplayed);
+    ++currentDisplayed;
+    console.log(currentDisplayed);
+    jQuery(".backgroundColored img").attr("src", images[currentDisplayed]);
+    jQuery(".backgroundColored .backgroundColored .col-copy h6").text(
+      texts[currentDisplayed]
+    );
+  });
 
-    jQuery(".arrow right").on("click", function () {
-        console.log(currentDisplayed);
-        ++currentDisplayed;
-        console.log(currentDisplayed);
-
-    });
+  jQuery(".arrow right").on("click", function () {
+    console.log(currentDisplayed);
+    ++currentDisplayed;
+    console.log(currentDisplayed);
+  });
 }
 
-String.prototype.replaceThisByThat = function(look, replacement) { 
-    var chars = this.split(look);
-    // chars[index] = replacement;
-    return chars.join(replacement);
-}
+String.prototype.replaceThisByThat = function (look, replacement) {
+  var chars = this.split(look);
+  // chars[index] = replacement;
+  return chars.join(replacement);
+};
